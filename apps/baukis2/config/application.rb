14db6e84dff57a6c351137daa351e1e7e423e072
Wataru Kuriyama <wataru.kuriyama@appirits.com>
2024-05-29 19:55:43 +0900

    migrationの作成まで

--- apps/baukis2/config/application.rb
+++ apps/baukis2/config/application.rb
@@ -8,7 +8,7 @@ module Baukis2
   class Application < Rails::Application
     config.load_defaults 6.0
     config.time_zone = "Tokyo"
-    config.i18n.load_path += 
+    config.i18n.load_path +=
       Dir[Rails.root.join("config", "locales" , "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
 
@@ -16,7 +16,7 @@ module Baukis2
       g.skip_routes true
       g.helper false
       g.assets false
-      g.test_flamework :rspec
+      g.test_framework :rspec
       g.controller_specs false
       g.view_specs false
     end
